e702b54964910d805156294471e3cccf831e2937
harrykim14 <harrykim14@korea.ac.kr>
2021-01-03 06:18:45 +0900

    21. 01. 03
    Chapter 01 - Refactoring: The first example

---
+++ 01. extract function.js
@@ -0,0 +1,52 @@
+const invoice = require('./incoices.json')
+const plays = require('./plays.json');
+
+function amountFor(perf, play){
+    let thisAmount = 0;
+
+        // 새로운 요구사항에 의해 switch문은 점점 복잡해질 것
+        switch (play.type) {
+            case "tragedy" : // 비극
+                thisAmount = 40000;
+                if(perf.audience > 30) thisAmount += 1000 * (perf.audience - 30);
+                break;
+            case "comedy" : // 희극
+                thisAmount = 30000;
+                if(perf.audience > 20) thisAmount += 10000+ 500 * (perf.audience - 20);
+                thisAmount += 300 * perf.audience;
+                break;
+            case "default" : // 디폴트
+                throw new Error(`알 수 없는 장르: ${play.type}`);
+        }
+
+        return thisAmount;    
+}
+
+function statement(invoice, plays){
+    let totalAmount = 0;
+    let volumeCredits = 0;
+    let result = `청구 내역 (고객명: ${invoice[0].customer})\n`;
+    const format = new Intl.NumberFormat("en-US",
+    { style: "currency", currency: "USD", minimumFractionDigits: 2}).format;
+    
+    for (let perf of invoice[0].perfomances){
+        const play = plays[perf.playID];
+        
+        let thisAmount = amountFor(perf,play);
+        //포인트 적립
+        volumeCredits += Math.max(perf.audience - 30, 0);
+
+        // 관객 5명마다 추가 포인트 제공
+        if("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+
+        // 청구 내역 출력 : <<< 리팩터링 필요: HTML로 출력하는 기능이 필요 >>>
+        result += `${play.name}: ${format(thisAmount/100)} (${perf.audience}석)\n`;
+        totalAmount += thisAmount;
+    }
+    result += `총액: ${format(totalAmount/100)}\n`;
+    result += `적립 포인트: ${volumeCredits}점\n`;
+    return result;
+}
+
+
+console.log(statement(invoice, plays));
