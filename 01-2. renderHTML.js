6c98df45650488d37dbfbe7c12e9cfe4b99f5adc
harrykim14 <harrykim14@korea.ac.kr>
2021-01-03 16:06:01 +0900

    21. 01. 03
    Chapter 01: The first example

---
+++ 01-2. renderHTML.js
@@ -0,0 +1,32 @@
+const invoice = require('./invoice.json');
+const plays = require('./plays.json');
+const createStatementData = require('./01-3. logic wt class')
+
+function htmlStatement(invoice, plays){;
+    return renderHTML(createStatementData(invoice,plays));   
+}
+
+function renderHTML(data) {
+    let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+    result += "<table>\n";
+    result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>";
+
+    for (let perf of data.performances){
+        result += `<tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>`;
+        result += `<td>${usd(perf.amount)}</td></tr>\n`;
+   }
+
+   result += `</table>\n`;
+   result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+   result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
+   return result;
+
+    function usd(aNumber) {
+        return new Intl.NumberFormat("en-US",
+         { style: "currency", currency: "USD", minimumFractionDigits: 2}).format(aNumber/100);
+    }   
+
+}
+
+console.log(htmlStatement(invoice, plays));
+
